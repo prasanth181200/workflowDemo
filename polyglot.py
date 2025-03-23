a45d83f012adcefcfb12eb10e4eb765711f0462c
Prasanth K <Prasanth181200@gmail.com>
2025-03-23 10:35:40 +0530

    first commit.

---
+++ polyglot.py
@@ -0,0 +1,6 @@
+msg = "This is a message!";
+
+1 // (lambda: eval("""print('Hello from Python!')""") or 1)()
+1 // (lambda: eval(f"""print("{msg}")""") or 1)()
+lambda: eval("console.log('Hello from JS!')")
+lambda: eval("console.log(msg)")
